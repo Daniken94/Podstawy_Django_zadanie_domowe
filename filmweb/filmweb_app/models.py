074599f9ee6681ae020150f5d46e80fcf3f52fe8
Kamil Klekotka <kamil.klekotka@gmail.com>
2021-10-06 12:00:16 +0200

    Poprawa modułów i bazy danych

--- filmweb/filmweb_app/models.py
+++ filmweb/filmweb_app/models.py
@@ -12,17 +12,26 @@ class Movies(models.Model):
         (5, "*****"),
     )
 
+    GENRE_CHOICES = (
+        (0, "Nie wiem"),
+        (1, "Komedia"),
+        (2, "Musical"),
+        (3, "Familijny"),
+        (4, "Film akcji"),
+        (5, "Przygodowy"),
+        (6, "Horror"),
+    )
     movie_name = models.CharField(max_length=255)
     director = models.CharField(max_length=255)
     year_of_production = models.IntegerField(null=True)
     rating = models.IntegerField(choices=CHOICES)
-    genre = models.IntegerField(null=True)
+    genre = models.IntegerField(choices=GENRE_CHOICES)
 
 class Actors(models.Model):
     GENDER_CHOISES = (
-        (-1, "I DONT KNOW"),
-        (0, "MAN"),
-        (1, "WOMAN"),
+        (-1, "Nie chcę podawać"),
+        (0, "Mężczyzna"),
+        (1, "Kobieta"),
     )
     actor_name = models.CharField(max_length=255)
     year_of_birth = models.DateField()
